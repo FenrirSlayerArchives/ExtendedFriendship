4efb42f9eb1d1c901aba64a6ab3ea94e7e35df48
FenrirSlayer <fenrirslayerarchives@gmail.com>
2024-05-04 13:59:32 +0200

    First commit. Empty/basic files

---
+++ ModEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace ExtendedFriendship
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
